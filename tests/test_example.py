98789c61d58a1cd1a371e6b5eaa0350470a224aa
OhWenChi <wenchi9318@gmail.com>
2025-05-22 00:30:52 +1000

    Added tests/ folder

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,1 +1,1 @@
-"import unittest\n\nclass TestExample(unittest.TestCase):\n    def test_pass(self):\n        self.assertTrue(True)" 
+"import unittest\nclass TestExample(unittest.TestCase):\n    def test_pass(self): self.assertTrue(True)" 
